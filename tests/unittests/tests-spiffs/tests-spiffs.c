06c41b7175db06fffdd519daf8babd5835d9490d
Joakim Nohlgård <joakim.nohlgard@eistec.se>
2016-07-14 11:46:56 +0200

    squash unittests/spiffs: Update to match vfs_mount changes

--- tests/unittests/tests-spiffs/tests-spiffs.c
+++ tests/unittests/tests-spiffs/tests-spiffs.c
@@ -31,28 +31,36 @@ static struct spiffs_desc spiffs_desc = {
     .dev = &_dev,
 };
 
+static vfs_mount_t _test_spiffs_mount = {
+    .fs = &spiffs_file_system,
+    .mount_point = "/test-spiffs",
+    .private_data = &spiffs_desc,
+};
+
 static void test_spiffs_mount_umount(void)
 {
-    int res = vfs_mount(&spiffs_file_system, "/", &spiffs_desc);
-    TEST_ASSERT(res >= 0);
+    int res;
+    res = vfs_mount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(res);
-    TEST_ASSERT(res >= 0);
+    res = vfs_umount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 }
 
 static void tests_spiffs_open_close(void)
 {
-    int mp = vfs_mount(&spiffs_file_system, "/", &spiffs_desc);
-    TEST_ASSERT(mp >= 0);
+    int res;
+    res = vfs_mount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    int res = vfs_open("/test.txt", O_CREAT | O_RDWR, 0);
+    res = vfs_open("/test-spiffs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(res >= 0);
 
     res = vfs_close(res);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(mp);
-    TEST_ASSERT(res >= 0);
+    res = vfs_umount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 }
 
 static void tests_spiffs_write(void)
@@ -60,26 +68,27 @@ static void tests_spiffs_write(void)
     const char buf[] = "TESTSTRING";
     char r_buf[2 * sizeof(buf)];
 
-    int mp = vfs_mount(&spiffs_file_system, "/", &spiffs_desc);
-    TEST_ASSERT(mp >= 0);
+    int res;
+    res = vfs_mount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    int fd = vfs_open("/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-spiffs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
-    int res = vfs_write(fd, buf, sizeof(buf));
+    res = vfs_write(fd, buf, sizeof(buf));
     TEST_ASSERT_EQUAL_INT(sizeof(buf), res);
 
     res = vfs_lseek(fd, 0, SEEK_SET);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_read(fd, r_buf, sizeof(r_buf));
     TEST_ASSERT(res == sizeof(buf));
     TEST_ASSERT_EQUAL_STRING(&buf[0], &r_buf[0]);
 
     res = vfs_close(fd);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    fd = vfs_open("/test.txt", O_RDONLY, 0);
+    fd = vfs_open("/test-spiffs/test.txt", O_RDONLY, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_read(fd, r_buf, sizeof(r_buf));
@@ -87,10 +96,10 @@ static void tests_spiffs_write(void)
     TEST_ASSERT_EQUAL_STRING(&buf[0], &r_buf[0]);
 
     res = vfs_close(fd);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(mp);
-    TEST_ASSERT(res >= 0);
+    res = vfs_umount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 }
 
 static void tests_spiffs_write2(void)
@@ -133,23 +142,24 @@ static void tests_spiffs_unlink(void)
 {
     const char buf[] = "TESTSTRING";
 
-    int mp = vfs_mount(&spiffs_file_system, "/", &spiffs_desc);
-    TEST_ASSERT(mp >= 0);
+    int res;
+    res = vfs_mount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    int fd = vfs_open("/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-spiffs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
-    int res = vfs_write(fd, buf, sizeof(buf));
+    res = vfs_write(fd, buf, sizeof(buf));
     TEST_ASSERT(res == sizeof(buf));
 
     res = vfs_close(fd);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test.txt");
-    TEST_ASSERT(res == 0);
+    res = vfs_unlink("/test-spiffs/test.txt");
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(mp);
-    TEST_ASSERT(res >= 0);
+    res = vfs_umount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 }
 
 static void tests_spiffs_readdir(void)
@@ -158,19 +168,20 @@ static void tests_spiffs_readdir(void)
     const char buf1[] = "TESTTESTSTRING";
     const char buf2[] = "TESTSTRINGSTRING";
 
-    int mp = vfs_mount(&spiffs_file_system, "/", &spiffs_desc);
-    TEST_ASSERT(mp >= 0);
+    int res;
+    res = vfs_mount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    int fd0 = vfs_open("/test0.txt", O_CREAT | O_RDWR, 0);
+    int fd0 = vfs_open("/test-spiffs/test0.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd0 >= 0);
 
-    int fd1 = vfs_open("/test1.txt", O_CREAT | O_RDWR, 0);
+    int fd1 = vfs_open("/test-spiffs/test1.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd1 >= 0);
 
-    int fd2 = vfs_open("/a/test2.txt", O_CREAT | O_RDWR, 0);
+    int fd2 = vfs_open("/test-spiffs/a/test2.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd2 >= 0);
 
-    int res = vfs_write(fd0, buf0, sizeof(buf0));
+    res = vfs_write(fd0, buf0, sizeof(buf0));
     TEST_ASSERT(res == sizeof(buf0));
 
     res = vfs_write(fd1, buf1, sizeof(buf1));
@@ -180,47 +191,47 @@ static void tests_spiffs_readdir(void)
     TEST_ASSERT(res == sizeof(buf2));
 
     res = vfs_close(fd0);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_close(fd1);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_close(fd2);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     vfs_DIR dirp;
-    res = vfs_opendir(&dirp, "/");
+    res = vfs_opendir(&dirp, "/test-spiffs");
 
     vfs_dirent_t entry;
     res = vfs_readdir(&dirp, &entry);
     TEST_ASSERT(res == 1);
-    TEST_ASSERT_EQUAL_STRING("test0.txt", &(entry.d_name[0]));
+    TEST_ASSERT_EQUAL_STRING("/test0.txt", &(entry.d_name[0]));
 
     res = vfs_readdir(&dirp, &entry);
     TEST_ASSERT(res == 1);
-    TEST_ASSERT_EQUAL_STRING("test1.txt", &(entry.d_name[0]));
+    TEST_ASSERT_EQUAL_STRING("/test1.txt", &(entry.d_name[0]));
 
     res = vfs_readdir(&dirp, &entry);
     TEST_ASSERT(res == 1);
-    TEST_ASSERT_EQUAL_STRING("a/test2.txt", &(entry.d_name[0]));
+    TEST_ASSERT_EQUAL_STRING("/a/test2.txt", &(entry.d_name[0]));
 
     res = vfs_readdir(&dirp, &entry);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_closedir(&dirp);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test0.txt");
-    TEST_ASSERT(res == 0);
+    res = vfs_unlink("/test-spiffs/test0.txt");
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test1.txt");
-    TEST_ASSERT(res == 0);
+    res = vfs_unlink("/test-spiffs/test1.txt");
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/a/test2.txt");
-    TEST_ASSERT(res == 0);
+    res = vfs_unlink("/test-spiffs/a/test2.txt");
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(mp);
-    TEST_ASSERT(res >= 0);
+    res = vfs_umount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 }
 
 static void tests_spiffs_rename(void)
@@ -228,49 +239,50 @@ static void tests_spiffs_rename(void)
     const char buf[] = "TESTSTRING";
     char r_buf[2 * sizeof(buf)];
 
-    int mp = vfs_mount(&spiffs_file_system, "/", &spiffs_desc);
-    TEST_ASSERT(mp >= 0);
+    int res;
+    res = vfs_mount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    int fd = vfs_open("/test.txt", O_CREAT | O_RDWR, 0);
+    int fd = vfs_open("/test-spiffs/test.txt", O_CREAT | O_RDWR, 0);
     TEST_ASSERT(fd >= 0);
 
-    int res = vfs_write(fd, buf, sizeof(buf));
+    res = vfs_write(fd, buf, sizeof(buf));
     TEST_ASSERT(res == sizeof(buf));
 
     res = vfs_lseek(fd, 0, SEEK_SET);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_read(fd, r_buf, sizeof(r_buf));
     TEST_ASSERT(res == sizeof(buf));
     TEST_ASSERT_EQUAL_STRING(&buf[0], &r_buf[0]);
 
     res = vfs_close(fd);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_rename("/test.txt", "/test1.txt");
-    TEST_ASSERT(res == 0);
+    res = vfs_rename("/test-spiffs/test.txt", "/test-spiffs/test1.txt");
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    fd = vfs_open("/test.txt", O_RDONLY, 0);
+    fd = vfs_open("/test-spiffs/test.txt", O_RDONLY, 0);
     TEST_ASSERT(fd < 0);
 
-    fd = vfs_open("/test1.txt", O_RDONLY, 0);
+    fd = vfs_open("/test-spiffs/test1.txt", O_RDONLY, 0);
     TEST_ASSERT(fd >= 0);
 
     res = vfs_lseek(fd, 0, SEEK_SET);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
     res = vfs_read(fd, r_buf, sizeof(r_buf));
     TEST_ASSERT(res == sizeof(buf));
     TEST_ASSERT_EQUAL_STRING(&buf[0], &r_buf[0]);
 
     res = vfs_close(fd);
-    TEST_ASSERT(res == 0);
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_unlink("/test1.txt");
-    TEST_ASSERT(res == 0);
+    res = vfs_unlink("/test-spiffs/test1.txt");
+    TEST_ASSERT_EQUAL_INT(0, res);
 
-    res = vfs_umount(mp);
-    TEST_ASSERT(res >= 0);
+    res = vfs_umount(&_test_spiffs_mount);
+    TEST_ASSERT_EQUAL_INT(0, res);
 }
 
 Test *tests_spiffs_tests(void)
